ece5223735494dc58c2b1fc50e211e4624c066fa
triplewy <triplewy@gmail.com>
2019-01-26 16:13:16 -0500

    Added stitch

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Stitch, RemoteMongoClient, AnonymousCredential } from 'mongodb-stitch-browser-sdk'
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.appId = 'brown-scheduler-lyrwc'
+
+  }
+  componentDidMount() {
+    this.client = Stitch.initializeAppClient(this.appId)
+    this.mongodb = this.client.getServiceClient(RemoteMongoClient.factory, 'mongodb-atlas').db('brownDB')
+    this.client.auth.loginWithCredential(new AnonymousCredential())
+      // this.mongodb
+      //   .collection('courses')
+      //   .find()
+      //   .asArray())
+
+
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
