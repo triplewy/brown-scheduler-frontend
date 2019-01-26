8bb1ea22305ceb506baf18f1ed5f585eaa5fabc4
Leon Lei <leon_lei@brown.edu>
2019-01-26 18:37:13 -0500

    rec courses

--- src/RecommendedCourses.js
+++ src/RecommendedCourses.js
@@ -1,22 +1,48 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
 import { Stitch, RemoteMongoClient, AnonymousCredential } from 'mongodb-stitch-browser-sdk'
 
 class RecommendedCourses extends Component {
   constructor(props) {
     super(props)
+    this.getCourses = this.getCourses.bind(this);
   }
 
+
+  getCourses(e) {
+    this.props.mongodb.collection('courses')
+      .find()
+      .toArray()
+      .then(data => {
+        console.log(data);
+      });
+  }
+
+  componentDidMount() {
+    // getCourses();
+    console.log(this.props.courses);
+  }
 
   render() {
 
     return (
       <div className="recommended-courses">
       <h1>Recommended Courses</h1>
-
-        
+      <div></div>        
       </div>
     );
   }
 }
 
-export default RecommendedCourses;
+function mapStateToProps(state) {
+  return {
+    ...state.app
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(RecommendedCourses);
