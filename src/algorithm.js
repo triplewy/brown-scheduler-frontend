01986dbb035127b3bf137f870bab4c481e18d1f9
sch3 <steven.c.ho@gmail.com>
2019-01-27 01:31:46 -0500

    Added requirements not taken to output. Streamlined logic a bit.

--- src/algorithm.js
+++ src/algorithm.js
@@ -10,6 +10,7 @@ export function algorithm(courses, concentrations, year, concentration, pathways
         'Computational Biology':7,
         'Design':8
     }
+    console.log(takenCourses_param)
     let takenCourses = []
     if(takenCourses_param&& takenCourses_param.length >0){
         for(var i = 0;i<takenCourses_param.length;i++){
@@ -30,6 +31,7 @@ export function algorithm(courses, concentrations, year, concentration, pathways
     var results = []
     var side_results = []// for recommendations, basically the second or third or class
     //console.log(concentration)
+    var requirements_not_taken = []
     if(concentration.requirements){
         // eliminate intro courses if necessary
         var intro = concentration.requirements.intro
@@ -49,21 +51,27 @@ export function algorithm(courses, concentrations, year, concentration, pathways
                 done.push(taken);
             }
             let i = done.indexOf(Math.max(...done));
-            console.log(i)
+            let any_added_intro = false
             for(var c of intro[keys[i]]){
                 console.log(c)
                 if(!takenCourses.includes(c)){
-                    
                     results.push(c)
+                    any_added_intro = true
                 }
+            }
+            if(!any_added_intro){
+                requirements_not_taken.push("Intro Series")
             }
         }
         
         //pathways check, just suggest all untaken intermediate classes
         let pathways_json = concentration.requirements.pathways
         console.log(pathways_json)
+        let total_intermediates = 0
+        let taken_intermediates = []
         for(var index in pathways){
             //if no core, add earliest core class
+            let path_done = true;
             let cur_pathway = pathways[index]
             let conc_pathway = pathways_json[pathways_mapping[cur_pathway]]
             console.log(cur_pathway);
@@ -79,11 +87,12 @@ export function algorithm(courses, concentrations, year, concentration, pathways
             }
             //default is first if no core
             if(!core_taken){
+                path_done = false
                 results.push(conc_pathway.Core[0])
             }
             //check intermediates
             let conc_intermediates = conc_pathway.Intermediate
-            let taken_intermediates = []
+            
             for (var conc_i in conc_intermediates){
                 let intermediate_class = conc_intermediates[conc_i]
                 if(intermediate_class.includes(' or ')){
@@ -99,6 +108,7 @@ export function algorithm(courses, concentrations, year, concentration, pathways
                         }
                     }
                     if(!took_one){
+                        path_done = false;
                         results.push(class_array[0])
                         //add remainder for recs
                         for(var class_index in class_array.slice(1)){
@@ -109,46 +119,55 @@ export function algorithm(courses, concentrations, year, concentration, pathways
                 }
                 else{
                     if(!takenCourses||!takenCourses.includes(intermediate_class)){
+                        path_done = false
                         results.push(intermediate_class);
                     }
                 }
             }
-            //recommend intermediates
-            let total_intermediates = taken_intermediates.length
-            //check if one from each category
-            let intermediate_categories = concentration.requirements["Intermediate Courses"]
-            console.log(intermediate_categories)
-            for(var cat in intermediate_categories){
-                let cat_list = intermediate_categories[cat]
-                if(cat==="Mathematics"){ //or contains OR, but I haven't seen another one yet
-                    cat_list = []
-                    for(var or_i in cat_list){
-                        let split = cat_list[or_i].split(' or ');
-                        for(var j in split){
-                            cat_list.push(split[j]);
-                        }
-                    }
-                }
-                let taken_cat = false;
-                for(var t_i in taken_intermediates){
-                    let taken_i = taken_intermediates[t_i]
-                    if(cat_list.includes(taken_i)){
-                        taken_cat = true
-                        break
-                    }
-                }
-                if(!taken_cat){
-                    results.concat(cat_list[0])
-                    side_results.concat(cat_list.slice(1))
-                }
+            if(!path_done){
+                requirements_not_taken.push('Pathway '+cur_pathway+ ' not done')
             }
             
         }
+        //recommend intermediates and increment taken intermeds
+        total_intermediates += taken_intermediates.length
         
-        
+        //check if one from each category
+        let intermediate_categories = concentration.requirements["Intermediate Courses"]
+        console.log(intermediate_categories)
+        for(var cat in intermediate_categories){
+            let cat_list = intermediate_categories[cat]
+            if(cat==="Mathematics"){ //or contains OR, but I haven't seen another one yet
+                cat_list = []
+                for(var or_i in cat_list){
+                    let split = cat_list[or_i].split(' or ');
+                    for(var j in split){
+                        cat_list.push(split[j]);
+                    }
+                }
+            }
+            let taken_cat = false;
+            for(var t_i in taken_intermediates){
+                let taken_i = taken_intermediates[t_i]
+                if(cat_list.includes(taken_i)){
+                    taken_cat = true
+                    break
+                }
+            }
+            if(!taken_cat){
+                requirements_not_taken.push("Class from intermediate category: "+cat + " not taken")
+                results.concat(cat_list[0])
+                side_results.concat(cat_list.slice(1))
+            }
+        }
+        if(total_intermediates<5){
+            requirements_not_taken.push("Total intermediate <5")
+        }
     }
+    
     //return 2D array each array denoting semester
-    let finalresult = {'results':results, 'recs':side_results}
+    console.log(requirements_not_taken)
+    let finalresult = {'results':results, 'recs':side_results, 'reqs_not_taken':requirements_not_taken}
     console.log(finalresult)
     return finalresult;
 }
