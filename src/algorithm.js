8af47ac06d495ee97b133bcecfbfde26bdcc17d9
sch3 <steven.c.ho@gmail.com>
2019-01-27 09:54:44 -0500

    Added ECON-AB to algo.

--- src/algorithm.js
+++ src/algorithm.js
@@ -10,6 +10,7 @@ export function algorithm(courses, concentrations, year, concentration, pathways
         'Computational Biology':7,
         'Design':8
     }
+    
     let takenCourses = []
     for(var i = 0; i < takenCourses_param.length; i++){
       if (takenCourses_param[i]){
@@ -30,149 +31,181 @@ export function algorithm(courses, concentrations, year, concentration, pathways
     //console.log(concentration)
     var requirements_not_taken = []
     let untaken_kv = {}
+    
     if (concentration.requirements) {
-        // eliminate intro courses if necessary
-        var intro = concentration.requirements.intro
-        var done = []
-        if (takenCourses.indexOf('CSCI 0170') >= 0 || takenCourses.indexOf('CSCI 0180') >= 0) {
-          results = concentration.requirements.intro.series2.filter(item => takenCourses.indexOf(item) < 0)
-        } else {
-          results = concentration.requirements.intro.series1.filter(item => takenCourses.indexOf(item) < 0)
-        }
-        // if (takenCourses.length == 0) {
-        //     results.push(concentration.requirements.intro.series1)
-        // } else {
-        //     var keys = Object.keys(intro);
-        //     for(var i=0;i<keys.length;i++){
-        //         var intro_series = intro[keys[i]];
-        //         var taken = 0
-        //         takenCourses.every(function(val) {  if(intro_series.indexOf(val)>=0){
-        //             taken+=1
-        //         } });
-        //         done.push(taken);
-        //     }
-        //     let i = done.indexOf(Math.max(...done));
-        //     let any_added_intro = false
-        //     for(var c of intro[keys[i]]){
-        //         console.log(c)
-        //         if(!takenCourses.includes(c)){
-        //             results.push(c)
-        //             any_added_intro = true
-        //         }
-        //     }
-        //     if(!any_added_intro){
-        //         requirements_not_taken.push("Intro Series")
-        //     }
-        // }
-
-        //pathways check, just suggest all untaken intermediate classes
-        let pathways_json = concentration.requirements.pathways
-        console.log(pathways_json)
-        let total_intermediates = 0
-        let taken_intermediates = []
-        for(var index in pathways){
-            //if no core, add earliest core class
-            let path_done = true;
-            let cur_pathway = pathways[index]
-            let conc_pathway = pathways_json[pathways_mapping[cur_pathway]]
-            console.log(cur_pathway);
-            console.log(conc_pathway);
-            // core check
-            let core_taken = false;
-            for(var core_index in conc_pathway.Core){
-                let core_class = conc_pathway.Core[core_index]
-                if(takenCourses && takenCourses.includes(core_class)){
-                    core_taken = true;
-                    break;
+        if(concentration.code=="ECON-AB"){
+            //rest of code is for CS SCB
+             console.log(concentration)
+            let econ_reqs = concentration.requirements
+            for(var econ_class of concentration.requirements.Economics){
+                if(econ_class.includes(' or ')){
+                    let econ_class_or = econ_class.split(' or ')
+                    let taken = true;
+                    for(var econ_class_split_class of econ_class_or){
+                        if(!takenCourses.includes(econ_class_or)){
+                            taken = false
+                        }
+                    }
+                    if(!taken){
+                        results.push(econ_class_or[0])
+                        side_results.push(econ_class_or[1])
+                        requirements_not_taken.push(econ_class_or+' requirement not taken')
+                    }
+                }else{
+                    if(!takenCourses.includes(econ_class)){
+                        requirements_not_taken.push(econ_class+' requirement not taken')
+                        results.push(econ_class)
+                    }
                 }
             }
-            //default is first if no core
-            if(!core_taken){
-                path_done = false
-                results.push(conc_pathway.Core[0])
+            if(!takenCourses.includes('ECON 0170')){
+                results.push('ECON 0170')
+                requirements_not_taken.push('Math requirement not taken')
             }
-            //check intermediates
-            let conc_intermediates = conc_pathway.Intermediate
+        }else{
+            // eliminate intro courses if necessary
+            var intro = concentration.requirements.intro
+            var done = []
+            if (takenCourses.indexOf('CSCI 0170') >= 0 || takenCourses.indexOf('CSCI 0180') >= 0) {
+              results = concentration.requirements.intro.series2.filter(item => takenCourses.indexOf(item) < 0)
+            } else {
+              results = concentration.requirements.intro.series1.filter(item => takenCourses.indexOf(item) < 0)
+            }
+            // if (takenCourses.length == 0) {
+            //     results.push(concentration.requirements.intro.series1)
+            // } else {
+            //     var keys = Object.keys(intro);
+            //     for(var i=0;i<keys.length;i++){
+            //         var intro_series = intro[keys[i]];
+            //         var taken = 0
+            //         takenCourses.every(function(val) {  if(intro_series.indexOf(val)>=0){
+            //             taken+=1
+            //         } });
+            //         done.push(taken);
+            //     }
+            //     let i = done.indexOf(Math.max(...done));
+            //     let any_added_intro = false
+            //     for(var c of intro[keys[i]]){
+            //         console.log(c)
+            //         if(!takenCourses.includes(c)){
+            //             results.push(c)
+            //             any_added_intro = true
+            //         }
+            //     }
+            //     if(!any_added_intro){
+            //         requirements_not_taken.push("Intro Series")
+            //     }
+            // }
 
-            for (var conc_i in conc_intermediates){
-                let intermediate_class = conc_intermediates[conc_i]
-                if(intermediate_class.includes(' or ')){
-                    let class_array = intermediate_class.split(' or ');
-                    let took_one =false;
-                    for(var class_index in class_array){
-                        let array_class = class_array[class_index]
-                        if(takenCourses&&takenCourses.includes(array_class)){
-                            took_one = true;
-                            if(!taken_intermediates.includes(array_class)){
-                                taken_intermediates.push(array_class);
+            //pathways check, just suggest all untaken intermediate classes
+            let pathways_json = concentration.requirements.pathways
+            console.log(pathways_json)
+            let total_intermediates = 0
+            let taken_intermediates = []
+            for(var index in pathways){
+                //if no core, add earliest core class
+                let path_done = true;
+                let cur_pathway = pathways[index]
+                let conc_pathway = pathways_json[pathways_mapping[cur_pathway]]
+                console.log(cur_pathway);
+                console.log(conc_pathway);
+                // core check
+                let core_taken = false;
+                for(var core_index in conc_pathway.Core){
+                    let core_class = conc_pathway.Core[core_index]
+                    if(takenCourses && takenCourses.includes(core_class)){
+                        core_taken = true;
+                        break;
+                    }
+                }
+                //default is first if no core
+                if(!core_taken){
+                    path_done = false
+                    results.push(conc_pathway.Core[0])
+                }
+                //check intermediates
+                let conc_intermediates = conc_pathway.Intermediate
+
+                for (var conc_i in conc_intermediates){
+                    let intermediate_class = conc_intermediates[conc_i]
+                    if(intermediate_class.includes(' or ')){
+                        let class_array = intermediate_class.split(' or ');
+                        let took_one =false;
+                        for(var class_index in class_array){
+                            let array_class = class_array[class_index]
+                            if(takenCourses&&takenCourses.includes(array_class)){
+                                took_one = true;
+                                if(!taken_intermediates.includes(array_class)){
+                                    taken_intermediates.push(array_class);
+                                }
+                            }
+                        }
+                        if(!took_one){
+                            path_done = false;
+                            results.push(class_array[0])
+                            //add remainder for recs
+                            for(var class_index in class_array.slice(1)){
+                                let array_class = class_array[class_index]
+                                side_results.push(array_class);
                             }
                         }
                     }
-                    if(!took_one){
-                        path_done = false;
-                        results.push(class_array[0])
-                        //add remainder for recs
-                        for(var class_index in class_array.slice(1)){
-                            let array_class = class_array[class_index]
-                            side_results.push(array_class);
+                    else{
+                        if(!takenCourses||!takenCourses.includes(intermediate_class)){
+                            path_done = false
+                            results.push(intermediate_class);
                         }
                     }
                 }
-                else{
-                    if(!takenCourses||!takenCourses.includes(intermediate_class)){
-                        path_done = false
-                        results.push(intermediate_class);
+                let untaken_related = []
+                let related_pathway_classes = conc_pathway.Related
+                console.log(related_pathway_classes)
+                for(var related_p_class of related_pathway_classes){
+                    if(!takenCourses.includes(related_p_class)&&!untaken_related.includes(related_p_class)){
+                        untaken_related.push(related_p_class)
                     }
                 }
-            }
-            let untaken_related = []
-            let related_pathway_classes = conc_pathway.Related
-            console.log(related_pathway_classes)
-            for(var related_p_class of related_pathway_classes){
-                if(!takenCourses.includes(related_p_class)&&!untaken_related.includes(related_p_class)){
-                    untaken_related.push(related_p_class)
+                untaken_kv[cur_pathway]= untaken_related
+
+                if(!path_done){
+                    requirements_not_taken.push('Pathway '+cur_pathway+ ' not done')
                 }
-            }
-            untaken_kv[cur_pathway]= untaken_related
-            
-            if(!path_done){
-                requirements_not_taken.push('Pathway '+cur_pathway+ ' not done')
-            }
 
-        }
-        //recommend intermediates and increment taken intermeds
-        total_intermediates += taken_intermediates.length
+            }
+            //recommend intermediates and increment taken intermeds
+            total_intermediates += taken_intermediates.length
 
-        //check if one from each category
-        let intermediate_categories = concentration.requirements["Intermediate Courses"]
-        console.log(intermediate_categories)
-        for(var cat in intermediate_categories){
-            let cat_list = intermediate_categories[cat]
-            if(cat==="Mathematics"){ //or contains OR, but I haven't seen another one yet
-                cat_list = []
-                for(var or_i in cat_list){
-                    let split = cat_list[or_i].split(' or ');
-                    for(var j in split){
-                        cat_list.push(split[j]);
+            //check if one from each category
+            let intermediate_categories = concentration.requirements["Intermediate Courses"]
+            console.log(intermediate_categories)
+            for(var cat in intermediate_categories){
+                let cat_list = intermediate_categories[cat]
+                if(cat==="Mathematics"){ //or contains OR, but I haven't seen another one yet
+                    cat_list = []
+                    for(var or_i in cat_list){
+                        let split = cat_list[or_i].split(' or ');
+                        for(var j in split){
+                            cat_list.push(split[j]);
+                        }
                     }
                 }
-            }
-            let taken_cat = false;
-            for(var t_i in taken_intermediates){
-                let taken_i = taken_intermediates[t_i]
-                if(cat_list.includes(taken_i)){
-                    taken_cat = true
-                    break
+                let taken_cat = false;
+                for(var t_i in taken_intermediates){
+                    let taken_i = taken_intermediates[t_i]
+                    if(cat_list.includes(taken_i)){
+                        taken_cat = true
+                        break
+                    }
+                }
+                if(!taken_cat){
+                    requirements_not_taken.push("Class from intermediate category: "+cat + " not taken")
+                    results.push(cat_list[0])
+                    side_results.concat(cat_list.slice(1))
                 }
             }
-            if(!taken_cat){
-                requirements_not_taken.push("Class from intermediate category: "+cat + " not taken")
-                results.push(cat_list[0])
-                side_results.concat(cat_list.slice(1))
+            if(total_intermediates<5){
+                requirements_not_taken.push("Total intermediate <5")
             }
-        }
-        if(total_intermediates<5){
-            requirements_not_taken.push("Total intermediate <5")
         }
     }
 
